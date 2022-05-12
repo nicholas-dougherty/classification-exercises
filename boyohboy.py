96af6473f72d19266b2dcfff339ac2727f1a1e83
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-12 12:46:08 -0500

    fixed errors in prepare

--- boyohboy.py
+++
@@ -1,1 +0,0 @@
-# Just checking some issues with git 
--- prepare.py
+++ prepare.py
@@ -1,15 +1,25 @@
+#                 ___    ___       ____      ___     _        ___       ____                    #
+#                F _ ", F _ ",    F ___J    F _ ",  /.\      F _ ",    F ___J                   #
+#               J `-' |J `-'(|   J |___:   J `-' | //_\\    J `-'(|   J |___:                   #
+#               |  __/F|  _  L   | _____|  |  __/F/ ___ \   |  _  L   | _____|                  #
+#               F |__/ F |_\  L  F L____:  F |__// L___J \  F |_\  L  F L____:                  #
+#              J__|   J__| \\__LJ________LJ__|  J__L   J__LJ__| \\__LJ________L                 #
+#              |__L   |__|  J__||________||__L  |__L   J__||__|  J__||________|                 #
+#|---------------------------------------------------------------------------------------------|#
+#|---------------------------------------------------------------------------------------------|#
+# IMPORTS
 import pandas as pd
 import numpy as np
-
+#|---------------------------------------------------------------------------------------------|#
 def prep_iris(df):
     df = df.drop(columns='species_id').rename(columns={'species_name': 'species'})
     #species_as_dummies = pd.get_dummies(df.species, drop_first=True)
     #df = pd.concat([df, species_as_dummies], axis=1)
     return df
-
+#|---------------------------------------------------------------------------------------------|#
 def prep_titanic(df):
     '''
-    takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned dataframe
+    takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned DF
     arguments: df: a pandas DataFrame with the expected feature names and columns
     return: clean_df: a dataframe with the cleaning operations performed on it
     '''
@@ -22,16 +32,17 @@ def prep_titanic(df):
     # encode categorical variables
     df_dummies = pd.get_dummies(df[['embark_town', 'sex']], dummy_na=False, drop_first=True)
     df = pd.concat([df, df_dummies], axis=1)
-    df = df.rename(columns={'embark_town_Queenstown': 'Queenstown', 'embark_town_Southampton': 'Southampton', 'sex_male': 'male'})
+    df = df.rename(columns={'embark_town_Queenstown': 'Queenstown', 'embark_town_Southampton':
+                            'Southampton', 'sex_male': 'male'})
     # df = df.drop(columns=['sex', 'embark_town'])
     return df.drop(columns=['sex', 'embark_town'])
-
-
+#|---------------------------------------------------------------------------------------------|#
 def prep_telco(df):
     # drop unnecessary, unhelpful, or duplicate columns. 
-    df = df.drop(columns=['customer_id', 'payment_type_id', 'internet_service_type_id',
-                                'contract_type_id', 'total_charges', 'tech_support', 'streaming_tv',
-                                'streaming_movies', 'online_backup', 'online_security'])
+    df = df.drop(columns=['customer_id', 'payment_type_id', 'internet_service_type_id', 
+                          'contract_type_id', 'total_charges', 'tech_support', 
+                          'streaming_tv', 'streaming_movies', 'online_backup',
+                          'online_security'])
     # Encode the categorical variables. 
     df['is_male'] = np.where(df.gender == 'Male', 1, 0)
     df['has_partner'] = np.where(df.partner == 'Yes', 1, 0)
@@ -46,11 +57,11 @@ def prep_telco(df):
     df['autopay'] = np.where(df.payment_type.str.contains('automatic') == True, 1, 0)
     df['churn'] = np.where(df.churn == 'Yes', 1, 0)
     # drop redundant, categorical variables. 
-    df = df.drop(columns=['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines',
-                                'internet_service_type', 'contract_type', 'payment_type', 'paperless_billing',
-                                'device_protection'])
+    df = df.drop(columns['gender','partner','dependents','phone_service','multiple_lines',
+                         'internet_service_type', 'contract_type', 'payment_type',
+                         'paperless_billing','device_protection'])
     return df 
-
+#|---------------------------------------------------------------------------------------------|#
 def prep_titanic_keep_age(df):
     '''
     takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned dataframe
@@ -66,8 +77,8 @@ def prep_titanic_keep_age(df):
     # encode categorical variables
     df_dummies = pd.get_dummies(df[['embark_town', 'sex']], dummy_na=False, drop_first=True)
     df = pd.concat([df, df_dummies], axis=1)
-    df = df.rename(columns={'embark_town_Queenstown': 'Queenstown', 'embark_town_Southampton': 'Southampton', 'sex_male': 'male'})
+    df = df.rename(columns={'embark_town_Queenstown': 'Queenstown',
+                            'embark_town_Southampton': 'Southampton', 'sex_male': 'male'})
     df.age = df.age.fillna(df.age.mean())
     return df.drop(columns=['sex', 'embark_town'])
-
-    
+#|---------------------------------------------------------------------------------------------|#
